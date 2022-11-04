b3264be4fdb544c8887f8c4bacf3056b923de43d
Elturco23 <jrianoa@uninorte.edu.co>
2022-11-04 16:33:31 -0500

    terminado

--- routeTimeline.js
+++ routeTimeline.js
@@ -8,18 +8,22 @@ router.get('/:id_usuario', async (req,res) => {
     // CREACION DE DOCUMENTO SEGUN MODELO
     let todospublicaciones=[];
     const usuariosseguidos= await UserModel.findById(id_usuario,{seguidos:1,_id:0});
-   const publicaciones=usuariosseguidos.seguidos.map(async (id)=>{ 
-    return await PublicacionModel.find({id_usuario:id},{id_usuario:1,titulo:1,descripcion:1,createdAt:1},
-        )
+   const publicaciones=usuariosseguidos.seguidos
+   for (let index = 0; index < publicaciones.length; index++) {
+  const id=publicaciones[index]
+   todospublicaciones[index] = await (await PublicacionModel.find({id_usuario:id},{id_usuario:1,titulo:1,descripcion:1,createdAt:1}));
+    
+   }
+  todospublicaciones=todospublicaciones.flat()
+   function arrayordenado(array){
+   return array.sort(
+    function(a, b){ 
+    return   new Date(b.createdAt).getTime()-new Date(a.createdAt).getTime()}
+    )
+   }
+   // ordenado desde la publicacion mas reciente a la mas antigua
+   res.json(arrayordenado(todospublicaciones))
   } 
     )
-    Promise.all(publicaciones).then((allpublicaciones)=>{
-        res.json(allpublicaciones)
-    })
-    
 
-  
-
-    
-})
 module.exports = router;
